f26cfdc0e2c87b7a5047aee3e48c32b9887aa068
nanabanayo <nanabanayo028@gmail.com>
2026-04-07 13:52:11 +0800

    zzz

--- admin.js
+++ admin.js
@@ -728,3 +728,4 @@ const dialogOverlay = document.getElementById('customDialog');
             const link = document.createElement("a"); link.href = URL.createObjectURL(blob); link.download = filename;
             document.body.appendChild(link); link.click(); document.body.removeChild(link);
         }
+    
